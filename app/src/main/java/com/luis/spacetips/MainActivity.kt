55bf946217f651330b4fbeac6cb1b7735f71551e
Lordmcd <luisguilhermemcd@gmail.com>
2024-10-17 03:59:10 -0300

    SUMMARY XML ID CHANGES Kotlin DONE

--- app/src/main/java/com/luis/spacetips/MainActivity.kt
+++ app/src/main/java/com/luis/spacetips/MainActivity.kt
@@ -31,38 +31,57 @@ class MainActivity : AppCompatActivity() {
 
             val tipTemp = binding.etMsTip.text
             val billTemp = binding.etMsBill.text
+            val personsTemp = binding.etMsPersons.text
 
 
 
-            if(tipTemp.isNotEmpty() && billTemp.isNotEmpty() ) {
+            if(tipTemp.isNotEmpty() && billTemp.isNotEmpty() && personsTemp.isNotEmpty()) {
 
                 val tipStr: String = binding.etMsTip.text.toString()
                 val billStr: String = binding.etMsBill.text.toString()
+                val personsStr: String = binding.etMsPersons.text.toString()
 
                 val tipValue = tipStr.toFloat()
                 val billValue = billStr.toFloat()
+                val personValue = personsStr.toInt()
 
                 val percentage = tipValue / 100
                 val tipAmount = billValue * percentage
+                val billWithTips = tipAmount + billValue
+                val billPerPerson = billWithTips / personValue
+
+                val tipPercentage: Int = tipTemp.toString().toInt()
 
 
 
                 val intent = Intent(this, SummaryActivity::class.java)
                 intent.apply {
-                    putExtra("tipResult", tipAmount)
-                    putExtra("tipTemp", tipTemp)
+                    putExtra("billWithTips", billWithTips)
+                    putExtra("tipTemp", tipPercentage)
+                    putExtra("billValue", billValue)
+                    putExtra("personAmount", personValue)
+                    putExtra("billPerPerson", billPerPerson)
+
 
                 }
                 startActivity(intent)
 
-            } else if(tipTemp.isEmpty() && billTemp.isNotEmpty()) {
+            } else if(tipTemp.isEmpty() && billTemp.isNotEmpty() && personsTemp.isNotEmpty()) {
                 Snackbar.make(binding.root, "Tip cannot be empty", Snackbar.LENGTH_SHORT).show()
-
-            } else if(billTemp.isEmpty() && tipTemp.isNotEmpty()) {
+            } else if(billTemp.isNotEmpty() && tipTemp.isNotEmpty() && personsTemp.isEmpty()) {
+                Snackbar.make(binding.root, "Persons cannot be empty", Snackbar.LENGTH_SHORT).show()
+            }else if(billTemp.isEmpty() && tipTemp.isNotEmpty() && personsTemp.isNotEmpty()) {
                 Snackbar.make(binding.root, "Bill cannot be empty", Snackbar.LENGTH_SHORT).show()
 
-            } else if(billTemp.isEmpty() && tipTemp.isEmpty()) {
+
+            } else if(billTemp.isEmpty() && tipTemp.isEmpty() && personsTemp.isNotEmpty()) {
                 Snackbar.make(binding.root, "Tip and Bill cannot be empty", Snackbar.LENGTH_SHORT).show()
+            } else if(billTemp.isEmpty() && tipTemp.isNotEmpty() && personsTemp.isEmpty()) {
+                Snackbar.make(binding.root, "Bill and Persons cannot be empty", Snackbar.LENGTH_SHORT).show()
+            } else if(billTemp.isNotEmpty() && tipTemp.isEmpty() && personsTemp.isEmpty()) {
+                Snackbar.make(binding.root, "Tip and Persons cannot be empty", Snackbar.LENGTH_SHORT).show()
+            } else if(billTemp.isNotEmpty() && tipTemp.isNotEmpty() && personsTemp.isEmpty()) {
+                Snackbar.make(binding.root, "Persons cannot be empty", Snackbar.LENGTH_SHORT).show()
             }
 
         }
@@ -70,6 +89,7 @@ class MainActivity : AppCompatActivity() {
         binding.cleanButton.setOnClickListener {
             binding.etMsTip.setText("")
             binding.etMsBill.setText("")
+            binding.etMsPersons.setText("")
 
 
         }
--- app/src/main/java/com/luis/spacetips/SummaryActivity.kt
+++ app/src/main/java/com/luis/spacetips/SummaryActivity.kt
@@ -5,15 +5,39 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.luis.spacetips.databinding.ActivityMainBinding
+import com.luis.spacetips.databinding.ActivitySummaryBinding
 
 class SummaryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySummaryBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_summary)
 
-        val tipAmount = intent.getFloatExtra("tipResult", 0.0f)
+        binding = ActivitySummaryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val billWithTips = intent.getFloatExtra("billWithTips", 0.0f)
         val tipPercentage = intent.getIntExtra("tipTemp", 0)
+        val personAmount = intent.getIntExtra("personAmount", 0)
+        val billWithoudTips = intent.getFloatExtra("billValue", 0.0f)
+        val billPerPerson = intent.getFloatExtra("billPerPerson", 0.0f)
+
+        binding.tvBillWithouttips.setText(billWithoudTips.toString() + "$")
+        binding.tvBillperperson.setText(billPerPerson.toString())
+        binding.tvBillWithtips.setText(billWithTips.toString())
+        binding.tvTipPercentage.setText(tipPercentage.toString() + "%")
+        binding.tvPersonsAmount.setText(personAmount.toString())
+
+
+        binding.refreshBtn.setOnClickListener {
+            finish()
+
+        }
+
+
 
     }
 }
